7dbe2c07219bdf9d70c02191f611de4682fb209b
Riko Pernando <rikopernando96@gmail.com>
2018-04-16 20:57:28 +0700

    CRUD with SQLIte => tapi belum pakai mvc, yg mvc segera menyusul
    CRUD with SQLIte => tapi belum pakai mvc, yg mvc segera menyusul

--- app.js
+++ app.js
@@ -21,8 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/add-products', indexRouter);
-app.use('/proses-add-produk', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
